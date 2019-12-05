a956653a9340eed38d39f8c62286b7eb0c07ea66
40823145 <40823145@gm.nfu.edu.tw>
2019-12-05 20:32:38 +0800

    溫度轉換-4

--- downloads/dart_ex/tp-1/slithery.dart
+++ downloads/dart_ex/tp-1/slithery.dart
@@ -1,4 +1,3 @@
-/*40823125
 import "dart:html";
 void main() {
   querySelector("#submit").onClick.listen((e) => calcRabbits());
@@ -18,64 +17,3 @@ int calculateRabbits(int c) {
       .round()
       .toInt();
 }
-*/
-import "dart:html";
-void main() {
-  querySelector("#submit").onClick.listen((e) => calcRabbits());
-}
-calcRabbits() {
-  // binding variables to html elements:
-  InputElement cInput = querySelector("#c");
-  LabelElement output = querySelector("#output");
-  // getting input
-  String cString = cInput.value;
-  int c = int.parse(cString);
-  // calculating and setting output:
-  output.innerHtml = "${calculateRabbits(c)}";
-}
-int calculateRabbits(int c) {
-   return ((9/5)*c+32)
-      .round()
-      .toInt();
-}
-/*40823125html
-After <input type="number" id="years" value="5" min="1" max="30"> Years
-<input type="button" id="submit" value="Calculate"/>
-<br/>Number of rabbits: <label id="output"></label>
-css
-body {
-  color: white;
-  font-size: 30px;
-}
-input, select, textarea {
-font-size: 100%;
-}
-*/
-/*40823125import 'dart:html';
-InputElement task;
-UListElement list;
-main() {
-  task = querySelector('#task');
-  list = querySelector('#list');
-  task.onChange.listen((e) => addItem());
-}
-void addItem() {
-  var newTask = LIElement();
-  newTask.text = task.value;
-  task.value = '';
-  list.children.add(newTask);
-}
-*/
-/*40823125html
-  <input id="task" type="text" placeholder=
-  "What do you want to do?"/>
-  <ul id="list"/>
-css
-body {
-  color: white;
-  font-size: 30px;
-}
-input, select, textarea {
-font-size: 100%;
-}
-*/
